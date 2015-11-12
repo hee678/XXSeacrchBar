f714ef2daf549c1401e59148b47599061460b00b
xieguoxing <xieguoxing@tftiancai.com>
2015-11-12 23:41:59 +0800

    初始化项目。

---
+++ githubDemo/githubDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubDemo
+//
+//  Created by LittleKin on 15/11/12.
+//  Copyright © 2015年 LittleKin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubDemo/githubDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubDemo
+//
+//  Created by LittleKin on 15/11/12.
+//  Copyright © 2015年 LittleKin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
