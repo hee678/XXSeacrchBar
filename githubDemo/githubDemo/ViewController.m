4124102c4fd046dd7fc482d47e133387baa34253
xieguoxing <xieguoxing@tftiancai.com>
2015-11-19 22:38:59 +0800

    Added:新增tableview

--- githubDemo/githubDemo/ViewController.m
+++ githubDemo/githubDemo/ViewController.m
@@ -8,8 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+@property (nonatomic, strong) UITableView *tableView;
 @end
 
 @implementation ViewController
@@ -18,7 +18,38 @@
     [super viewDidLoad];
     self.title = @"测试git功能";
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"返回" style:UIBarButtonItemStylePlain target:self action:@selector(clickLeftBarButton)];
+    [self setUpTableView];
 }
+
+- (void)setUpTableView
+{
+    NSLog(@"self.view = %@",NSStringFromCGRect(self.view.frame));
+    NSLog(@"self.navigationBar = %@",NSStringFromCGRect(self.navigationController.navigationBar.frame));
+    NSLog(@"statusBarFrame = %@",NSStringFromCGRect([UIApplication sharedApplication].statusBarFrame));
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    self.tableView.backgroundColor = [UIColor orangeColor];
+    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    self.tableView.rowHeight = 50;
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.view addSubview:self.tableView];
+}
+
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"cell";
+      UITableViewCell *  cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+        cell.textLabel.text = [NSString stringWithFormat:@"第%zd行",indexPath.row];
+    return cell;
+}
+
 
 - (void)clickLeftBarButton
 {
